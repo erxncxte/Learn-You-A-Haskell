77503b53d29a996c5c9786df2b4273b39137dbb2
Erin Cate <77478941+erxncxte@users.noreply.github.com>
2021-01-18 10:33:53 +1000

    18/01

--- First-Functions.hs
+++ First-Functions.hs
@@ -1,1 +1,14 @@
 doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmall Number x = if x > 100
+                        then x
+                        else x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+-- Adding a ' denotes a strict version of a function or a slightly modified version
+
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
